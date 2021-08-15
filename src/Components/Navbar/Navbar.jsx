b22f871af8249eaabbe5243ac3e730c1d52c4993
Henry Tryban <htryban@gmail.com>
2021-08-14 23:08:15 -0500

    warnings

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({totalItems}) => {
 		<>
 			<AppBar position="fixed" className={classes.appBar} color="inherit">
 				<Toolbar>
-					<Typography component={Link} to="/" variant="h" className={classes.title} color="inherit">
+					<Typography component={Link} to="/" variant="h5" className={classes.title} color="inherit">
 						<img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
 						Opposite Day
 					</Typography>
