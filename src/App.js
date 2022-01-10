6436f38a6f7bfab4569de67c8edd429cbdc00b3d
Henry Tryban <32393901+htryban@users.noreply.github.com>
2022-01-09 23:17:50 -0600

    Style notistack (#22)
    * style notistack
    
    * style notistack

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Spinner from "./Components/Spinner/Spinner";
 import {SnackbarProvider} from 'notistack';
 import {initializeApp} from 'firebase/app'
 import {getAnalytics, logEvent} from 'firebase/analytics'
+import useStyles from './index'
 
 const theme = createTheme({
 	palette: {
@@ -67,6 +68,8 @@ const App = () => {
 		setProducts(data);
 	}
 
+	const classes = useStyles();
+
 	const fetchCart = async () => {
 		setCart(await commerce.cart.retrieve());
 	}
@@ -122,7 +125,12 @@ const App = () => {
 
 	return (
 		<ThemeProvider theme={theme}>
-			<SnackbarProvider maxSnack={3}>
+			<SnackbarProvider
+				maxSnack={3}
+				classes={{
+                    variantSuccess: classes.success,
+                    variantError: classes.error,
+                }}>
 				<Router>
 					<div style={{display: 'flex', justifyContent: 'center', alignContent: 'center', height: '100vh - 24px', width: '100vw - 24px'}}>
 						<Navbar totalItems={cart.total_items}/>
